1e69e9edd37d151307e95158da4b4474ecae3d66
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2026-01-02 23:32:29 +0700

    Menyediakan PenyediaViewModel dengan repository siswa

--- app/src/main/java/com/example/myfirebase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/provider/PenyediaViewModel.kt
@@ -1,3 +1,21 @@
 package com.example.myfirebase.viewmodel.provider
 
 
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.lifecycle.ViewModelProvider
+import com.example.myfirebase.repositori.AplikasiDataSiswa
+import com.example.myfirebase.viewmodel.EntryViewModel
+import com.example.myfirebase.viewmodel.HomeViewModel
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
+        )
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+    }
+}
