4fe2551d558c717c113dbf774e062e73ab09181d
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2026-01-02 23:23:09 +0700

    Menyiapkan DefaultContainerApp dengan FirebaseRepositorySiswa

--- app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
@@ -1,2 +1,13 @@
 package com.example.myfirebase.repositori
 
+import android.app.Application
+
+interface ContainerApp {
+    val repositorySiswa: RepositorySiswa
+}
+
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
