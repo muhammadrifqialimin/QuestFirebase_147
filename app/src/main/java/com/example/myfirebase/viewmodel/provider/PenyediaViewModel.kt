a44d9d2ba18a4d6751369f2d32e605bda53d7ea0
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2026-01-08 21:19:32 +0700

    inisialisasi HomeViewModel dengan repository siswa

--- app/src/main/java/com/example/myfirebase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/provider/PenyediaViewModel.kt
@@ -5,17 +5,26 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import com.example.myfirebase.repositori.AplikasiDataSiswa
 import com.example.myfirebase.viewmodel.EntryViewModel
 import com.example.myfirebase.viewmodel.HomeViewModel
+import com.example.myfirebase.viewmodel.DetailViewModel
+import com.example.myfirebase.viewmodel.EditViewModel
 
-fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
-        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
-        )
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+            AplikasiDataSiswa)
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer {
+            HomeViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
     }
 }
