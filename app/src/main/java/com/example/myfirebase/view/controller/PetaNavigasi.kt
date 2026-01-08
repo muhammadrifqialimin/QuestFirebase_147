8bb3dbc5a7be31b8bbc30c43d260b76bfd31ed71
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2026-01-08 21:15:55 +0700

    Menambahkan navigasi ke halaman entry menggunakan NavController.

--- app/src/main/java/com/example/myfirebase/view/controller/PetaNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/controller/PetaNavigasi.kt
@@ -1,19 +1,27 @@
 package com.example.myfirebase.view.controller
 
-import com.example.myfirebase.view.EntrySiswaScreen
-import com.example.myfirebase.view.HomeScreen
-import com.example.myfirebase.view.route.DestinasiDetail
-import com.example.myfirebase.view.route.DestinasiEntry
-import com.example.myfirebase.view.route.DestinasiHome
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.myfirebase.view.DetailSiswaScreen
+import com.example.myfirebase.view.EditSiswaScreen
+import com.example.myfirebase.view.EntrySiswaScreen
+import com.example.myfirebase.view.HomeScreen
+import com.example.myfirebase.view.route.DestinasiDetail
+import com.example.myfirebase.view.route.DestinasiEdit
+import com.example.myfirebase.view.route.DestinasiEntry
+import com.example.myfirebase.view.route.DestinasiHome
 
 @Composable
-fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier = Modifier) {
+fun DataSiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+) {
     HostNavigasi(navController = navController)
 }
 
@@ -25,16 +33,19 @@ fun HostNavigasi(
     NavHost(
         navController = navController,
         startDestination = DestinasiHome.route,
-        modifier = modifier
+        modifier = Modifier
     ) {
         composable(DestinasiHome.route) {
             HomeScreen(
-                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiEntry.route)
+                },
                 navigateToItemUpdate = {
                     navController.navigate("${DestinasiDetail.route}/$it")
                 }
             )
         }
+
         composable(DestinasiEntry.route) {
             EntrySiswaScreen(
                 navigateBack = { navController.navigate(DestinasiHome.route) }
